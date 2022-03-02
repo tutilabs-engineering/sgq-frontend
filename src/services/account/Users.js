ef86938f52ad15c73e39549c72981734e1403f74
William David Martins de Almeida <wdma.lic17@uea.edu.br>
2022-03-02 17:30:00 -0400

    update service users

--- src/services/account/Users.js
+++ src/services/account/Users.js
@@ -22,6 +22,10 @@ export default {
 
     validate: () => {
         return http.get("validateToken")
+    },
+
+    updateUser: (dataUpdateUser) => {
+        return http.put("users", dataUpdateUser)
     }
 
 }
