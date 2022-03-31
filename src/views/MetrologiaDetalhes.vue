70cdd1c7fbd697fa29988fa254d37e5abab438ee
William David Martins de Almeida <wdma.lic17@uea.edu.br>
2022-03-31 09:33:53 -0400

    created metrologia detalhes

--- src/views/MetrologiaDetalhes.vue
+++ src/views/MetrologiaDetalhes.vue
@@ -1,20 +1,39 @@
 <template>
 
 <div class="content-metrologiaDetalhes">
-    <h2>METROLOGIA</h2>
+    
 
-    <div class="input-metrologiaDetalhes">
-        <InputMetrologia title="Código Produto" :type="text" :placeholder="'ex: 00.1234/01'"/>
+      <fieldset class="form">
+        <legend>Metrologia</legend>
 
-        <InputMetrologia title="Código Cliente" :type="text" :placeholder="'ex: Empresa 01'"/>
+      <div class="input">
+        <label for="op">Código Produto</label>
+        <input type="text" name="client" id="op" placeholder="Digite o código OP">
+      </div>
 
-        <InputMetrologia title="Produto" :type="text" :placeholder="'ex: Maquineta'"/>
+      <div class="input">
+        <label for="client">Código Cliente</label>
+        <input type="text" name="client" id="client" placeholder="ex: Yamaha">
+      </div>
 
-        <InputMetrologia title="Cliente" :type="text" :placeholder="'ex: 0154879/02'"/>
+      <div class="input">
+        <label for="client">Produto</label>
+        <input type="text" name="client" id="client" placeholder="ex: Yamaha">
+      </div>
 
-    </div>
+      <div class="input">
+        <label for="client">Cliente</label>
+        <input type="text" name="client" id="client" placeholder="ex: Yamaha">
+      </div>
 
-    <TableMetrologiaDetalhes />
+     </fieldset>
+
+     <fieldset>
+       <legend>Preenchimento</legend>
+
+      <TableMetrologiaDetalhes />
+
+    </fieldset>
 
 </div>
   
@@ -22,11 +41,10 @@
 
 <script>
 
-import InputMetrologia from '../components/InputsMetrologia/InputMetrologia.vue'
 import TableMetrologiaDetalhes from '../components/TableMetrologiaDetalhes/TableMetrologiaDetalhes.vue'
 
 export default {
-    components: {InputMetrologia, TableMetrologiaDetalhes},
+    components: { TableMetrologiaDetalhes},
     name: "MetrologiaDetalhes",
     data(){
         return {};
@@ -38,40 +56,78 @@ export default {
 
 <style scoped>
 
-.content-metrologiaDetalhes {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    width: 100%;
+legend {
+  font-size: 30px;
+  font-weight: 600;
+  color: var(--black_text);
 }
 
-.content-metrologiaDetalhes h2 {
-    margin-top: 20px;
-    text-align: center;
-    color: var(--black_text);
+
+fieldset {
+  border: 1px solid rgba(37, 36, 36, 0.281);
+  width: 100%;
+  background-color: white;
+  border-radius: 10px 10px 10px 10px;
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
 }
 
-.input-metrologiaDetalhes {
-    margin-top: 20px;
-    width: 70%;
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    grid-gap: 10px;
+.form {
+  padding: 20px;
+  width: 100%;
+  height: 100%;
+  background-color: white;
+  border-radius: 10px 10px 10px 10px;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 20px;
+  margin-bottom: 20px;
+}
+
+.input {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height: 70px;
+  padding: 5px;
+  border-radius: 5px 5px 0 0;
+}
+
+.input label {
+  font-weight: 600;
+  font-size: 18px;
+  color: var(--black_text);
+}
+
+.input input {
+  background-color: transparent;
+  border: none;
+  transition: 1s;
+  outline: none;
+  font-size: 15px;
+  height: 52px;
+  border-bottom: 2px solid rgba(128, 128, 128, 0.39);
 }
 
 @media (max-width: 1114px) and (min-width: 766px){
-    .input-metrologiaDetalhes {
-        width: 70%;
-        grid-template-columns: 1fr;
-    }
+  .legend {
+    text-align: center;
+  }
+    .form {
+    padding: 12px;
+    grid-template-columns: auto;
+  }
 }
 
 @media (max-width: 765px){
-    .input-metrologiaDetalhes {
-        width: 100%;
-        grid-template-columns: 1fr;
-    }
+  legend {
+    text-align: center;
+  }
+    .form {
+    padding: 12px;
+    grid-template-columns: 1fr;
+  }
 }
 
 </style>
