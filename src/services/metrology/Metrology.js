b0d61060e3729322b2115c8dde1caccf27626536
William David Martins de Almeida <wdma.lic17@uea.edu.br>
2022-04-05 07:37:55 -0400

    created metrology http

---
+++ src/services/metrology/Metrology.js
@@ -0,0 +1,15 @@
+import { http } from "../config";
+
+export default {
+    ListMetrologyHistory: async () => {
+        return await http.get("metrology/history")
+    },
+
+    ListMetrologySolicitations: async () => {
+        return await http.get("metrology/solicitations")
+    },
+
+    FindMetrologyById: async (id) => {
+        return await http.get(`metrology/${id}`)
+    }
+}
