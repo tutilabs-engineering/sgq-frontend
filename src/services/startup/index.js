2e67d92d48948b34c42912cf16d570e9fe6b977e
William David Martins de Almeida <wdma.lic17@uea.edu.br>
2022-04-07 10:58:17 -0400

    update router

--- src/services/startup/index.js
+++ src/services/startup/index.js
@@ -2,10 +2,10 @@ import { http } from "../config"
 
 export default {
   listDataByCodeOp: async (code_op) => {
-    return await http.get(`/dataOp/${code_op}`)
+    return await http.get(`/reportStartup/dataOp/${code_op}`)
   },
 
   listAllDefaultQuestions: async () => {
-    return http.get(`/defaultQuestions`)
+    return http.get(`/reportStartup/defaultQuestions`)
   }
 }
