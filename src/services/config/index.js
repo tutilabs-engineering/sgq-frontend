3d7060e80a259c74ce401c3e71882c37e6304f2c
Pablo <pabloguedesc@gmail.com>
2022-04-18 16:37:10 -0400

    add new dist and change the route for production

--- src/services/config/index.js
+++ src/services/config/index.js
@@ -2,46 +2,47 @@ import axios from "axios"
 
 //LOCAL
 
-const http = axios.create({
-    baseURL: "http://localhost:8000",
-    headers: {
-        "Accept": "application/json",
-        "Content": "application/json"
-    }
-})
-
-// API SAP
-
-const httpSAP = axios.create({
-  baseURL: "http://192.168.7.212:3300/api/v1/sap/tutilabs/ops/",
-  headers: {
-      "Accept": "application/json",
-      "Content": "application/json"
-  }
-})
-
-// SERVER
-
 // const http = axios.create({
-//     baseURL: "http://185.209.179.253:7600",
+//     baseURL: "http://localhost:8000",
 //     headers: {
 //         "Accept": "application/json",
 //         "Content": "application/json"
 //     }
 // })
 
+// API SAP
+
+const httpSAP = axios.create({
+  baseURL: "http://192.168.7.212:3300/api/v1/sap/tutilabs/ops/",
+  headers: {
+    "Accept": "application/json",
+    "Content": "application/json"
+  }
+})
+
+// SERVER
+
+const http = axios.create({
+  baseURL: "http://185.209.179.253:7600",
+  headers: {
+    "Accept": "application/json",
+    "Content": "application/json"
+  }
+})
+
 http.interceptors.request.use(function (config) {
-    let token = sessionStorage.getItem("token");
+  let token = sessionStorage.getItem("token");
 
-    if(!token) {
-        token = "";
-    }
+  if (!token) {
+    token = "";
+  }
 
-    config.headers.Authorization = `Bearer ${token}`;
-    return config;
+  config.headers.Authorization = `Bearer ${token}`;
+  return config;
 },
   (error) => {
-    console.log(error)  
-    return Promise.reject(error)} 
+    console.log(error)
+    return Promise.reject(error)
+  }
 )
 export { http, httpSAP };
