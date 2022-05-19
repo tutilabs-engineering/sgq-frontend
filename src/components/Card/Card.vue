724756a05edcd7d1dc1fe9fa76cbaf8745431590
William David Martins de Almeida <wdma.lic17@uea.edu.br>
2022-05-19 08:07:09 -0400

    update card

--- src/components/Card/Card.vue
+++ src/components/Card/Card.vue
@@ -1,20 +1,26 @@
 <template>
-  <Popper :content="textContentPopper" hover="true" placement="bottom">
+  <Popper
+    class="popper"
+    :content="textContentPopper"
+    hover="true"
+    placement="bottom"
+  >
     <router-link :to="link">
-      <div class="cardStatus" :style="{ color: colore, borderLeft: '7px solid' }">
+      <div
+        class="cardStatus"
+        :style="{ color: colore, borderLeft: '7px solid' }"
+      >
         <div className="cardStatusText">
           <h3>{{ status }}</h3>
           <spam>{{ qtde }}</spam>
         </div>
         <i :class="img" class="cardImg"></i>
       </div>
-      
     </router-link>
   </Popper>
 </template>
 
 <script>
-
 import Popper from "vue3-popper";
 
 export default {
@@ -30,19 +36,19 @@ export default {
 </script>
 
 <style scoped>
-
 :deep(.popper) {
-    background: var(--card_green);
-    padding: 5px;
-    border-radius: 5px;
-    color: #fff;
-    font-weight: bold;
-    text-transform: uppercase;
-    font-size: 10px;
-    height: 25px;
-  }
+  background: var(--card_green);
+  padding: 5px;
+  border-radius: 5px;
+  color: #fff;
+  font-weight: bold;
+  text-transform: uppercase;
+  font-size: 10px;
+  height: 25px;
+}
 
 a {
+  width: 100%;
   text-decoration: none;
 }
 
@@ -51,10 +57,9 @@ a {
   transform: scale(1.02);
 }
 
-
 .cardStatus {
   /* margin: 30px 10px 0px 10px; */
-  width: 270px;
+  width: 100%;
   height: 120px;
   background-color: var(--bg_white);
   border: 1px solid rgba(37, 36, 36, 0.281);
@@ -63,6 +68,13 @@ a {
   align-items: center;
   padding: 0px 25px 0px 25px;
   border-radius: 10px 5px 5px 10px;
+}
+
+.popper {
+  /* width: max(7rem, 30vw); */
+  width: 100%;
+  margin: 0 !important;
+  border: none !important;
 }
 
 .cardStatusText h3 {
@@ -80,12 +92,17 @@ a {
   font-size: 30px;
 }
 
+@media (max-width: 1100px) {
+  .popper {
+    width: 100%;
+
+  }
+}
+
 @media (max-width: 500px) {
   .cardStatus {
     width: 90vw;
   }
 }
-
-
 </style>
 
